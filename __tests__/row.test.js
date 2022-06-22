f52f99589fe937d5a7e636c19b081a8351527bfc
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-22 15:30:16 -0700

    should check if all items in row are from 1-9
    Co-Authored-By: Anthony DiFalco <106050281+Di-Falco@users.noreply.github.com>

--- __tests__/row.test.js
+++ __tests__/row.test.js
@@ -9,11 +9,21 @@ describe('Row', () => {
 
   test('should check if length is 9', () => {
     const sudoku = new Row([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    expect(sudoku.isValid()).toEqual(true);
+    expect(sudoku.isLength()).toEqual(true);
   });
 
   test('should check if length is not 9', () => {
     const sudoku = new Row([1, 2, 3, 4, 5, 6]);
-    expect(sudoku.isValid()).toEqual(false);
+    expect(sudoku.isLength()).toEqual(false);
+  });
+
+  test('should check if all items in row are from 1-9', () => {
+    const sudoku = new Row([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(sudoku.isNumbers()).toEqual(true);
+  });
+
+  test('should check if an items in row isnt from 1-9', () => {
+    const sudoku = new Row(["a", 12, 0]);
+    expect(sudoku.isNumbers()).toEqual(false);
   });
 });
--- src/row.js
+++ src/row.js
@@ -2,10 +2,19 @@ export default function Row(sudoki){
   this.sudoki = sudoki;
 }
 
-Row.prototype.isValid = function() {
+Row.prototype.isLength = function() {
   if (this.sudoki.length === 9){
     return true;
   } else {
     return false;
   }
+}
+
+Row.prototype.isNumbers = function(){
+  let i = true
+  this.sudoki.forEach( function(element){
+    if (10 <= parseInt(element) || parseInt(element) <=0 || isNaN(parseInt(element))) {
+      i = false;
+    }});
+  return i;
 }
