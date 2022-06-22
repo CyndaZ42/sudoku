88f063031ee82f3a8f043f6865458ddf5e402fe5
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-22 14:59:52 -0700

    should create a row with numbers
    Co-Authored-By: Anthony DiFalco <106050281+Di-Falco@users.noreply.github.com>

--- __tests__/row.test.js
+++ __tests__/row.test.js
@@ -1,9 +1,9 @@
-import Row from './src/row.js'
+import Row from './../src/row.js'
 
 describe('Row', () => {
 
   test('should create a row with numbers', () => {
-    const sudoku = new Card([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    const sudoku = new Row([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     expect(sudoku.sudoki).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
